8a408ae635e5da012d2cf2be8d41230def147c52
Ciungu Vlad <ciunguvlad@gmail.com>
2024-04-24 19:58:28 +0300

    exercitiul 3

--- laborator/content/stiva/3-stack-addressing/stack-addressing.asm
+++ laborator/content/stiva/3-stack-addressing/stack-addressing.asm
@@ -12,21 +12,38 @@ main:
     ; TODO 1: replace every push by an equivalent sequence of commands (use direct addressing of memory. Hint: esp)
     mov ecx, NUM
 push_nums:
-    push ecx
+    sub esp, 4
+    mov dword [esp], ecx
     loop push_nums
 
-    push 0
-    push "mere"
-    push "are "
-    push "Ana "
+    sub esp, 4
+    mov dword [esp], 0
+
+    sub esp, 4
+    mov dword [esp], "mere"
+
+    sub esp, 4
+    mov dword [esp], "are "
+
+    sub esp, 4
+    mov dword [esp], "Ana "
 
     lea esi, [esp]
     PRINTF32 `%s\n\x0`, esi
 
     ; TODO 2: print the stack in "address: value" format in the range of [ESP:EBP]
     ; use PRINTF32 macro - see format above
+    mov eax, ebp
+print_stack:
+    PRINTF32 `0x%x: 0x%x\n\x0`, eax, [eax]
+
+    sub eax, 4
+    cmp eax, esp
+    jge print_stack
 
     ; TODO 3: print the string
+    lea esi, [esp]
+    PRINTF32 `%s\n\x0`, esi
 
     ; TODO 4: print the array on the stack, element by element.
 
